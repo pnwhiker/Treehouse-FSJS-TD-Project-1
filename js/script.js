2fc158fcfeba8b95077775eb7b13d6ef1bb950f8
PNWHiker <53063554+pnwhiker@users.noreply.github.com>
2020-09-24 14:27:47 -0700

    Progress on Scripts.js

--- js/script.js
+++ js/script.js
@@ -11,19 +11,61 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
+const quotesArray = [
+  {
+    quote:'test quotation 1',
+    author:'author 1',
+    citation:'source 1',
+    year:'year 1'
+  },
+  {
+    quote:'test quotation 2',
+    author:'author 2',
+    citation:'source ',
+    year:'year 2'
+  },
+  {
+    quote:'test quotation 3',
+    author:'author3 ',
+    citation:'source 3',
+    year:'year 3'
+  },
+  {
+    quote:'test quotation 4',
+    author:'author 4',
+    citation:'source 4',
+    year:'year 4'
+  },
 
+  
+
+
+];
+
+console.log(quotesArray);
+console.log(quotesArray.length);
 
 /***
  * `getRandomQuote` function
 ***/
 
+function getRandomQuote() {
+  const selectedNumber = Math.floor(Math.random() * quotesArray.length)+1;
+  const quote = quotesArray[selectedNumber];
+  return quote;
+  
+};
+
+const selectedQuote = getRandomQuote();
+
+console.log(selectedQuote);
 
 
 /***
  * `printQuote` function
 ***/
 
-
+function printQuote() {};
 
 /***
  * click event listener for the print quote button
